a78bb5a8b8d1ce4e022eab54c39c1980b2546524
jean <jean.davy@asperasoft.com>
2015-03-21 00:41:00 +0100

    on stage ...

--- examples/common.cmake
+++ examples/common.cmake
@@ -1,8 +1,24 @@
+# cmake -G "Visual Studio 12 2013"
+# cmake -G "Visual Studio 14 2015"
+#
+# cmake --build . 
+#
+# cmake --build . --config Release
+
 cmake_minimum_required (VERSION 3.2.1)
 
 set(Boost_USE_STATIC_LIBS ON) 
 set(Boost_USE_MULTITHREADED ON)  
 set(Boost_USE_STATIC_RUNTIME ON) 
+
+# set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib_[CONFIG])
+# set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib_[CONFIG])
+#PROJECT_BINARY_DIR contains the full path to the top level directory of your build tree
+
+#release build
+#cmake -DCMAKE_CXX_FLAGS_RELEASE --build ./
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
 
 find_package(Boost
 	1.57.0				# Minimum version
